9b1aecde91742591bf9cc6c28e5cf5d546e1d766
MRID0007 <mrid0007@student.monash.edu>
2024-04-09 23:18:37 +1000

    TASK 1 - Route.py Implementation

--- route.py
+++ route.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
-
 from computer import Computer
-
 from typing import TYPE_CHECKING, Union
 
 # Avoid circular imports for typing.
@@ -26,7 +24,8 @@ class RouteSplit:
 
     def remove_branch(self) -> RouteStore:
         """Removes the branch, should just leave the remaining following route."""
-        raise NotImplementedError()
+        return self.following.store
+
 
 @dataclass
 class RouteSeries:
@@ -45,34 +44,35 @@ class RouteSeries:
         Returns a route store which would be the result of:
         Removing the computer at the beginning of this series.
         """
-        raise NotImplementedError()
+        return self.following.store
 
     def add_computer_before(self, computer: Computer) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding a computer in series before the current one.
         """
-        raise NotImplementedError()
+        return RouteSeries(computer, Route(self))
 
     def add_computer_after(self, computer: Computer) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding a computer after the current computer, but before the following route.
         """
-        raise NotImplementedError()
+        return RouteSeries(self.computer, Route(RouteSeries(computer, self.following)))
 
     def add_empty_branch_before(self) -> RouteStore:
         """Returns a route store which would be the result of:
         Adding an empty branch, where the current routestore is now the following path.
         """
-        raise NotImplementedError()
+        return RouteSplit(Route(None), Route(None), Route(self))
 
     def add_empty_branch_after(self) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding an empty branch after the current computer, but before the following route.
         """
-        raise NotImplementedError()
+        new_following = Route(RouteSplit(Route(None), Route(None), self.following))
+        return RouteSeries(self.computer, new_following)
 
 
 RouteStore = Union[RouteSplit, RouteSeries, None]
@@ -80,7 +80,6 @@ RouteStore = Union[RouteSplit, RouteSeries, None]
 
 @dataclass
 class Route:
-
     store: RouteStore = None
 
     def add_computer_before(self, computer: Computer) -> Route:
@@ -88,14 +87,14 @@ class Route:
         Returns a *new* route which would be the result of:
         Adding a computer before everything currently in the route.
         """
-        raise NotImplementedError()
+        return Route(RouteSeries(computer, self))
 
     def add_empty_branch_before(self) -> Route:
         """
         Returns a *new* route which would be the result of:
         Adding an empty branch before everything currently in the route.
         """
-        raise NotImplementedError()
+        return Route(RouteSplit(Route(None), Route(None), self))
 
     def follow_path(self, virus_type: VirusType) -> None:
         """Follow a path and add computers according to a virus_type."""
@@ -103,4 +102,14 @@ class Route:
 
     def add_all_computers(self) -> list[Computer]:
         """Returns a list of all computers on the route."""
-        raise NotImplementedError()
+        def traverse(route_store):
+            if route_store is None:
+                return []
+            elif isinstance(route_store, RouteSplit):
+                return traverse(route_store.top.store) + traverse(route_store.bottom.store) + traverse(
+                    route_store.following.store)
+            elif isinstance(route_store, RouteSeries):
+                return [route_store.computer] + traverse(route_store.following.store)
+            return []
+
+        return traverse(self.store)
