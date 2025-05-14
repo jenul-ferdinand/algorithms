c0f2aa23b361220d1458b76ad3f955274b78f291
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2025-05-14 16:35:20 +1000

    initial setup for a2

---
+++ assignment2.py
@@ -0,0 +1,16 @@
+from typing import List, Tuple
+
+ClassNumber = int
+Allocation = List[ClassNumber]
+
+def crowdedCampus(
+    no_students: int, 
+    max_students: int, 
+    time_prefs: List[List[int]], 
+    proposed_classes, 
+    min_satisfaction
+):
+    pass
+
+if __name__ == '__main__':
+    pass
