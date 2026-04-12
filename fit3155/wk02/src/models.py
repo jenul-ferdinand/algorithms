9e113632f0e968529ad22b5be4a025bcf847cb0f
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-04-12 17:43:42 +1000

    add attributes to bm output dataclass

--- fit3155/wk02/src/models.py
+++ fit3155/wk02/src/models.py
@@ -3,7 +3,21 @@ from dataclasses import dataclass, field
 
 @dataclass
 class BMOutput:
+    """
+    Boyer Moore Algorithm Output
+    
+    Attributes:
+        match_positions: Array of positions where matches were made in the 
+        original string
+
+        matches: Count of pattern matches to the given string
+
+        comparisons: Count of comparisons made in total
+
+        shifts: Count of bad character shifts
+    """
     match_positions: list[int] = field(default_factory=list)
     matches: int = 0
     comparisons: int = 0
     shifts: int = 0
+    goodsuffix_array: list[int] = field(default_factory=list)
