bdc725089a4fd673543947a46d86643ac78b04cb
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-04-12 12:47:57 +1000

    better testing for zalg

--- fit3155/wk01/tests/test_naive.py
+++
@@ -1,8 +0,0 @@
-from fit3155.wk01.src.naive import naive
-
-def test_one_letter():
-    assert naive("a") == [1]
-
-
-def test_repeating():
-    assert naive("aaaaaaa") == [7, 6, 5, 4, 3, 2, 1]
--- fit3155/wk01/tests/test_zalg.py
+++ fit3155/wk01/tests/test_zalg.py
@@ -1,40 +1,94 @@
+from hypothesis import assume, given
+from hypothesis import strategies as st
+
+from fit3155.wk01.src.models import ZalgOutput
+from fit3155.wk01.src.naive import naive
 from fit3155.wk01.src.zalg import zalg
 
 
+@given(st.text(alphabet="ab", max_size=50))
+def test_zalg_vs_naive(txt):
+    zalg_out: ZalgOutput = zalg(txt)
+    naive_out: ZalgOutput = naive(txt)
+    assert zalg_out.z_array == naive_out.z_array
+    assert zalg_out.comparisons <= naive_out.comparisons
+
+
+@given(st.text(alphabet="ab", max_size=30))
+def test_naive_invariants(txt):
+    out: ZalgOutput = naive(txt)
+    n = len(txt)
+
+    assume(n > 1)
+
+    assert out.case1_times == n - 1
+    assert out.case2_times is None
+
+
+@given(st.text(alphabet="ab", max_size=100))
+def test_zalg_invariants(txt):
+    out: ZalgOutput = zalg(txt)
+    n = len(txt)
+
+    assume(n > 10)
+
+    # Every position k=1...n-1 is exactly one of Case 1 or Case 2
+    assert out.case1_times + out.case2_times == n - 1
+
+    # Every case 2 is either a reuse (2a) or a clamp (2b)
+    assert out.reuse_times + out.clamp_times == out.case2_times
+
+    # The algorithm is O(n), comparisons should not be more than 2n
+    assert out.comparisons < 2 * n
+
+
+funcs = [naive, zalg]
+
+
 def test_repeating():
-    assert zalg("aaaaaaa") == [7, 6, 5, 4, 3, 2, 1]
+    for f in funcs:
+        out: ZalgOutput = f("aaaaaaa")
+        assert out.z_array == [7, 6, 5, 4, 3, 2, 1]
 
 
-def test_nomatches():
-    assert zalg("abcde") == [5, 0, 0, 0, 0]
+def test_no_matches():
+    for f in funcs:
+        out: ZalgOutput = f("abcde")
+        assert out.z_array == [5, 0, 0, 0, 0]
 
 
 def test_empty_string():
-    assert zalg("") == []
+    for f in funcs:
+        out: ZalgOutput = f("")
+        assert out.z_array == []
 
 
-def test_single():
-    assert zalg("a") == [1]
+def test_single_character():
+    for f in funcs:
+        out: ZalgOutput = f("a")
+        assert out.z_array == [1]
 
 
-def test_notes():
-    assert zalg("aabcaabxaabcaabcay") == [
-        18,
-        1,
-        0,
-        0,
-        3,
-        1,
-        0,
-        0,
-        7,
-        1,
-        0,
-        0,
-        5,
-        1,
-        0,
-        0,
-        1,
-        0,
-    ]
+def test_examples_from_notes():
+    for f in funcs:
+        out: ZalgOutput = f("aabcaabxaabcaabcay")
+        assert out.z_array == [
+            18,
+            1,
+            0,
+            0,
+            3,
+            1,
+            0,
+            0,
+            7,
+            1,
+            0,
+            0,
+            5,
+            1,
+            0,
+            0,
+            1,
+            0,
+        ]
