d5681b8de01f48f9e4c9f2a4695703cb627c4a69
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-04-15 21:14:18 +1000

    update bm ouput model

--- fit3155/wk02/src/models.py
+++ fit3155/wk02/src/models.py
@@ -5,19 +5,41 @@ from dataclasses import dataclass, field
 class BMOutput:
     """
     Boyer Moore Algorithm Output
-    
+
     Attributes:
-        match_positions: Array of positions where matches were made in the 
+        match_positions: Array of positions where matches were made in the
         original string
 
         matches: Count of pattern matches to the given string
 
         comparisons: Count of comparisons made in total
 
-        shifts: Count of bad character shifts
+        matched_comparisons: Count of comparisons that matched in total.
+
+        shifts: Count of shifts in total
+
+        gs_shifts: Count of the shifts caused by the good suffix rule
+
+        mp_shifts: Count of the shifts caused by the matched prefix rule
+
+        bcr_shifts: Count of the shifts caused by the bad character rule
+
+        z_suffix: Z suffix array
+
+        goodsuffix: Good suffix array
     """
+
     match_positions: list[int] = field(default_factory=list)
     matches: int = 0
+
     comparisons: int = 0
-    shifts: int = 0
-    goodsuffix_array: list[int] = field(default_factory=list)
+    matched_comparisons: int = 0
+
+    total_shifts: int = 0
+    gs_shifts: int = 0
+    mp_shifts: int = 0
+    bcr_shifts: int = 0
+
+    z_suffix: list[int] = field(default_factory=list)
+    goodsuffix: list[int] = field(default_factory=list)
+    mp: list[int] = field(default_factory=list)
